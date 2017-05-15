5f4e2aabdb04ea00f7f9de02c97d10bacb7b8509
domin <dominikieltyka@wp.pl>
2017-05-15 10:56:44 +0200

    page navigation

---
+++ src/main/java/com/pkproject/bank/beans/CalendarManagedBean.java
@@ -0,0 +1,22 @@
+package com.pkproject.bank.beans;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.Date;
+
+/**
+ * Created by domin on 5/14/17.
+ */
+@ManagedBean
+@SessionScoped
+public class CalendarManagedBean {
+    private Date date = new Date();
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/pkproject/bank/beans/SearchBean.java
@@ -0,0 +1,15 @@
+package com.pkproject.bank.beans;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by domin on 5/14/17.
+ */
+@ManagedBean
+@SessionScoped
+public class SearchBean {
+    public void search() {
+
+    }
+}
